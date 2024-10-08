79d358f433018d21576d4959bcef8578f48959e7
Tim Lindquist <tml24@pct.edu>
2024-10-08 10:46:44 -0400

    weapon swapping + started ammo drops

---
+++ Monkey Wars Battle of Sargent Meeks/Assets/TimsFolder/Scripts/AmmoPickup.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AmmoPickup : MonoBehaviour
+{
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        
+    }
+}
